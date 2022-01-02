612c6a320aeb78b0a9d9d886f5152255e4144468
sformichella <sformichella@gmail.com>
2022-01-02 15:54:41 -0800

    add get for testing and log

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ import { verifyKeyMiddleware } from 'discord-interactions'
 const port = process.env.PORT;
 const publicKey = process.env.discordPublicKey
 
+app.get('/api/interactions', (req, res) => {
+  res.send('gaming goonery music bot')
+})
+
 app.post('/api/interactions', verifyKeyMiddleware(publicKey), (req, res) => {
   if(req.json.type === 1) res.send({ type: 1 })
   console.log('Interactions endpoint');
+  console.log('INTERACTIONS REQUEST', req);
 });
 
 app.listen(port, () => {
