feeab8e616184d9c9e8a2bab70367eaf8dd8b285
sformichella <sformichella@gmail.com>
2022-01-02 15:28:18 -0800

    add public key

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+// app.use(express.json());
+
+import { verifyKeyMiddleware } from 'discord-interactions-js'
+
+const {
+  verifyKeyMiddleware: verify
+} = require('discord-interactions-js')
 
 const port = process.env.port;
+const publicKey = process.env.discordPublicKey
 
-app.post('/api/interactions', (req, res) => {
+app.post('/api/interactions', verify(publicKey), (req, res) => {
+  if(req.json.type === 1) res.send({ type: 1 })
   console.log('Interactions endpoint');
 });
 
